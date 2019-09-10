a001378b55081f611f8cfb30206c9d404199af03
georgiana0818 <georgiana0818@gmail.com>
2019-09-10 01:52:29 -0400

    Add files via upload

---
+++ YahtzeeGame/src/main/java/YahtzeeGame/YahtzeeGame/App.java
@@ -0,0 +1,13 @@
+package YahtzeeGame.YahtzeeGame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
