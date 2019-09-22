5dab727557ac9920d5b70b69d12be36476083df1
Georgiana0818 <georgiana0818@github.com>
2019-09-22 03:49:54 -0400

    push1

--- YahtzeeGame/src/test/java/YahtzeeGame/YahtzeeGame/GameTest.java
+++ YahtzeeGame/src/test/java/YahtzeeGame/YahtzeeGame/GameTest.java
@@ -1,16 +1,68 @@
 package YahtzeeGame.YahtzeeGame;
 
-public class GameTest {
+import junit.framework.TestCase;
+
+public class GameTest extends TestCase{
 	
 	public void testYahtzee() {
-	  
+		  
 		Game tg = new Game();
-		assertEquals(true, tg.isYahtzee([5,5,5,5,5]));
+		int[] test = new int[] {5,5,5,5,5};
+		int[] test2 = new int[] {5,5,3,5,5};
+		assertEquals(50, tg.checkYahtzee(test));
+		assertEquals(0, tg.checkYahtzee(test2));
 		
 	}
 	
 	public void testFullHouse() {
 		Game tg = new Game();
-		assertEquals(true, tg.isFullHouse([1,2,1,2,1]));
+		int[] test = new int[] {1,2,1,2,1};
+		int[] test2 = new int[] {4,2,1,2,1};
+		assertEquals(25, tg.checkFullHouse(test));
+		assertEquals(0, tg.checkFullHouse(test2));
+	}
+	
+	public void testThreeKind() {
+		Game tg = new Game();
+		int[] test = new int[] {3,2,3,2,2};
+		int[] test2 = new int[] {3,2,3,2,1};
+		assertEquals(12, tg.checkThreeKind(test));
+		assertEquals(0, tg.checkThreeKind(test2));
+	}
+	
+	public void testFourKind() {
+		Game tg = new Game();
+		int[] test = new int[] {1,1,1,2,1};
+		int[] test2 = new int[] {1,1,1,1,1};
+		int[] test3 = new int[] {1,2,3,1,1};
+		assertEquals(6, tg.checkFourKind(test));
+		assertEquals(5, tg.checkFourKind(test2));
+		assertEquals(0, tg.checkFourKind(test3));
+	}
+	
+	public void testSmallStraight() {
+		Game tg = new Game();
+		int[] test = new int[] {1,2,3,4,2};
+		int[] test2 = new int[] {1,2,3,4,5};
+		int[] test3= new int[] {1,2,5,4,5};
+		assertEquals(30, tg.checkSmallStraight(test));
+		assertEquals(30, tg.checkSmallStraight(test2));
+		assertEquals(0, tg.checkSmallStraight(test3));
+	}
+	
+	public void testLargeStraight() {
+		Game tg = new Game();
+		int[] test = new int[] {1,2,3,4,5};
+		int[] test2 = new int[] {3,2,3,2,1};
+		assertEquals(40, tg.checkLargeStraight(test));
+		assertEquals(0, tg.checkLargeStraight(test2));
+	}
+	
+	public void testaddUpperSection() {
+		Game tg = new Game();
+		int[] test = new int[] {1,2,3,4,5};
+		int[] test2 = new int[] {2,2,3,4,5};
+		assertEquals(1, tg.addUpperSection(test, 1));
+		assertEquals(0, tg.addUpperSection(test2, 1));
 	}
 }
